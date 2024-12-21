68c5e0e235cffc567bfacc39290b6a88ccd7eb5f
MrHold <mrholdmb@gmail.com>
2024-12-21 15:14:38 +0300

    changed type of userid on increased BigInteger

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # models.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, BigInteger, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -7,12 +7,13 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, unique=True, index=True, nullable=False)
-    username = Column(String, nullable=True)
-    first_name = Column(String, nullable=True)
+    id = Column(BigInteger, primary_key=True, index=True)
+    user_id = Column(BigInteger, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=True)
-    variant_number = Column(Integer, default=0)  # Дефолтное значение, например, 0
+    username = Column(String, nullable=True)
+    variant_number = Column(Integer, default=0)  
 
     def __repr__(self):
-        return f"<User(user_id={self.user_id}, username={self.username}, variant_number={self.variant_number})>"
+        return (f"<User(id={self.id}, user_id={self.user_id}, first_name={self.first_name}, "
+                f"last_name={self.last_name}, username={self.username}, variant_number={self.variant_number})>")
